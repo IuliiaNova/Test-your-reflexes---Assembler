7c6fe0c460fad53702babe6def33a81401cc8559
Edgar Arcos <edgararcosmartin@gmail.com>
2022-12-02 14:00:24 +0000

    Solucion pasar primera pagina a segunda y reformulacion de error UserName

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ let gameUsername = document.querySelector("#gameUsername");
 let gameStartGame = document.querySelector("#gameStartGame");
 let gameGetReady = document.querySelector("#gameGetReady");
 let chooseUsernameInput = document.querySelector("#chooseUsernameInput")
+let invalidUsername = document.querySelector("#invalidUsername")
 let patternUsername = /[ `!@#$%^&*()+=\[\]{};':"\\|,.<>\/?]/;
 let dataObject = new Object();
 let counterValidationUsername = false;
@@ -29,15 +30,12 @@ chooseUsernameInput.addEventListener('focusin', invalidUsernameIn);
 
 function invalidUsernameOut() {
     if (patternUsername.test(chooseUsernameInput.value)||chooseUsernameInput.value=="") {
-        let text = "Invalid username. Do not use special characters.";
-        let paragraphInvalidUsername = document.createElement("p");
-        paragraphInvalidUsername.setAttribute("class", "color-paragraph-username");
-        paragraphInvalidUsername.innerHTML = `${text}`;
-        gameUsername.appendChild(paragraphInvalidUsername);
+        invalidUsername.classList.add("invalidUsernameAppear");
         chooseUsernameInput.style.border = "2px dashed red";
         counterValidationUsername = true;
     }
     else {
+        invalidUsername.classList.remove("invalidUsernameAppear");
         usernameCorrect=true;
     }
 }
@@ -45,8 +43,6 @@ function invalidUsernameOut() {
 function invalidUsernameIn() {
     if (counterValidationUsername == true) {
         chooseUsernameInput.style.border = "none";
-        let colorParagraphUsername = document.querySelector(".color-paragraph-username");
-        colorParagraphUsername.remove();
         counterValidationUsername = false;
     }
 }
@@ -57,5 +53,6 @@ function nextPageToRules(){
         localStorage.setItem("username", JSON.stringify(dataObject.username))
         windowGame.style.transitionDuration = "1s";
         windowGame.style.transform = "translateY(-90vh)";
+        event.preventDefault();
     } 
 }
