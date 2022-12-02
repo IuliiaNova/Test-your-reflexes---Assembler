e23828e596743479fc509c8d80f87b072e59308c
Edgar Arcos <edgararcosmartin@gmail.com>
2022-12-02 10:16:25 +0000

    Cambios Iniciales en Js

--- app.js
+++ app.js
@@ -4,14 +4,21 @@ let startGameButton = document.querySelector("#startGameButton");
 let gameUsername = document.querySelector("#gameUsername");
 let gameStartGame = document.querySelector("#gameStartGame");
 let gameGetReady = document.querySelector("#gameGetReady");
+let chooseUsernameInput = document.querySelector("#chooseUsernameInput")
+let patternUsername = /[ `!@#$%^&*()+=\[\]{};':"\\|,.<>\/?]/;
+let dataObject = new Object();
+let counterValidationUsername = false;
+let usernameCorrect = false;
 
-
-startButton.addEventListener("click", nextPage);
+startButton.addEventListener("click", nextPageToRules);
 startGameButton.addEventListener("click", startGame);
 
-function nextPage(){
+function nextPageToRules(){
     windowGame.style.transitionDuration = "1s";
     windowGame.style.transform = "translateY(-90vh)";
+    if (usernameCorrect = true) {
+        dataObject.username = chooseUsernameInput.value;
+    } 
 }
 
 function startGame(){
@@ -25,4 +32,29 @@ function getReady(){
     windowGame.style.transform = "translateY(-270vh)";
 }
 
+chooseUsernameInput.addEventListener('focusout', invalidUsernameOut);
+chooseUsernameInput.addEventListener('focusin', invalidUsernameIn);
 
+function invalidUsernameOut() {
+    if (patternUsername.test(chooseUsernameInput.value)) {
+        let text = "Invalid username. Do not use special characters.";
+        let paragraphInvalidUsername = document.createElement("p");
+        paragraphInvalidUsername.setAttribute("class", "color-paragraph-username");
+        paragraphInvalidUsername.innerHTML = `${text}`;
+        gameUsername.appendChild(paragraphInvalidUsername);
+        chooseUsernameInput.style.border = "2px dashed red";
+        counterValidationUsername = true;
+    }
+    else {
+        usernameCorrect=true;
+    }
+}
+
+function invalidUsernameIn() {
+    if (counterValidationUsername == true) {
+        chooseUsernameInput.style.border = "none";
+        let colorParagraphUsername = document.querySelector(".color-paragraph-username");
+        colorParagraphUsername.remove();
+        counterValidationUsername = false;
+    }
+}
