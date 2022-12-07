72df8eda776c34c1ad6594c72966b07f44a72e3f
Iuliia Shikhanova <yshihanova@gmail.com>
2022-12-07 00:01:51 +0000

    inner time

--- app.js
+++ app.js
@@ -22,12 +22,14 @@ const stopGameButtonThree = document.querySelector("#stopGameButtonThree");
 const playAgainButton = document.querySelector("#playAgainButton");
 const pushColor = document.querySelector("#pushColor");
 const gameResult = document.querySelector("#gameResult");
+const resultOne = document.querySelector("#resultOne");
 let startTime=new Date();
 let endTime=new Date();
 let startPressed=false;
 let gameHasStarted=false;
 let maxWait=20;
 let timerID;
+
 
 startGameButton.addEventListener("click", startGame);
 stopGameButtonOne.addEventListener("click", finishGame);
@@ -37,27 +39,19 @@ chooseUsernameInput.addEventListener('focusin', invalidUsernameIn);
 usernameButton.addEventListener("click", nextPageToRules);
 
 function playGame(){
-    //Set randon time - set color 
-    // If for buttons color&&next
-
-    let color = ["RED", "BLUE", "GREEN"]; //It should be random of three colors
-    let colorTextOut = document.createElement("p");
-    pushColor.appendChild(colorTextOut);
-    colorTextOut.innerHTML = `${color}`;
     startTime = new Date();
 }
 
 function stopGame(){
     if(gameHasStarted){
-        console.log("Hola")
         endTime=new Date();
-        let responseTime=(endTime.getTime()-startTime.getTime()-5000)/1000;
+        let responseTime=(endTime.getTime()-startTime.getTime()-3000)/1000;
         gameHasStarted=false;
-        let yourResult = document.createElement("p");
-        yourResult.innerText = ("Your response time is: " + responseTime +
+        gameResult.innerText = ("Your response time is: " + responseTime +
         " seconds " + "\n" + remark(responseTime));
-        gameResult.appendChild(yourResult);
-        pushColor.removeChild(colorTextOut); //no funciona
+        resultOne.innerText = responseTime; 
+        /* let yourResult = document.createElement("p"); */
+        /* gameResult.appendChild(you}rResult); */ 
     }
     else{
         clearTimeout(timerID);
@@ -140,12 +134,13 @@ function startGame(){
     windowGame.style.gridTemplateRows= "100% 100% 100%";
     gameGetReady.style.display = "grid";
     gameHasStarted=true;
-    playGame();
+    
 }
 
 function hideGetReady(){
     getReadyCountDown.style.display = "none";
     threeCountDown.style.display = "grid";
+    playGame();
 }
 
 function showTwoCountDown(){
